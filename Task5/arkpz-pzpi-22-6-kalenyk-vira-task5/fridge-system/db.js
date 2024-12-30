6540c93b556af5a6eb7e4f8f0eb15a4e7279c4c4
NureKalenykVira <129483483+NureKalenykVira@users.noreply.github.com>
2024-12-30 13:05:36 +0200

    Update db.js

--- Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
+++ Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
@@ -1,24 +1,19 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'fridge_system',
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
-  queueLimit: 0
-});
+try {
+    const pool = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    });
+    console.log('Database connected successfully.');
+    return connection;
+} catch (error) {
+    console.error('Database connection failed:', error.message);
+}
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Database connection failed:', err.message);
-  } else {
-    console.log('Database connected successfully');
-    connection.release();
-  }
-});
 
 module.exports = pool.promise();
