3bde7cd40cf3d21f5069a61aef12f9fbf9b71112
NureKalenykVira <129483483+NureKalenykVira@users.noreply.github.com>
2024-12-30 13:29:53 +0200

    Update db.js

--- Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
+++ Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
@@ -2,14 +2,13 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'fridge_system',
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
-  queueLimit: 0
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
 });
 
 pool.getConnection((err, connection) => {
