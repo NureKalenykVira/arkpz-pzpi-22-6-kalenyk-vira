351a1d18cfb97cd87cb05867b2c75cf627e00cec
NureKalenykVira <129483483+NureKalenykVira@users.noreply.github.com>
2024-12-30 15:40:31 +0200

    Update db.js

--- Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
+++ Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // {{RAILWAY_PRIVATE_DOMAIN}}
-    user: process.env.DB_USER, // root
-    password: process.env.DB_PASSWORD, // ваш пароль
-    database: process.env.DB_NAME, // railway
-    port: process.env.DB_PORT, // 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
 });
