6aa9504f505d47f7f5ba08ed73c3368962903467
NureKalenykVira <129483483+NureKalenykVira@users.noreply.github.com>
2024-12-30 13:10:54 +0200

    Update db.js

--- Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
+++ Task5/arkpz-pzpi-22-6-kalenyk-vira-task5/fridge-system/db.js
@@ -1,19 +1,25 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-try {
-    const pool = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-    });
-    console.log('Database connected successfully.');
-    return connection;
-} catch (error) {
-    console.error('Database connection failed:', error.message);
+async function createPool() {
+    try {
+        const pool = await mysql.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+            waitForConnections: true,
+            connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+        });
+        console.log('Database connected successfully.');
+        return pool;
+    } catch (error) {
+        console.error('Failed to connect to the database:', error.message);
+        throw error;
+    }
 }
 
+const pool = createPool();
 
-module.exports = pool.promise();
+module.exports = pool;
